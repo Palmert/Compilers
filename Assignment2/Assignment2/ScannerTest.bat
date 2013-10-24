c2c8ad5a0bf1e4aa400060df6238e26ebc865913
Thom Palmer <thomapalmer@gmail.com>
2013-10-24 19:50:28 -0400

    Final Submission files

--- Assignment2/Assignment2/ScannerTest.bat
+++ Assignment2/Assignment2/ScannerTest.bat
@@ -1,4 +1,8 @@
-E:\GitHub\Compilers\Assignment2\Debug\Assignment2.exe  ass2w.pls > ass2w.out
-E:\GitHub\Compilers\Assignment2\Debug\Assignment2.exe  ass2r.pls > ass2r.out
-E:\GitHub\Compilers\Assignment2\Debug\Assignment2.exe  a2error.pls > a2error.out
-E:\GitHub\Compilers\Assignment2\Debug\Assignment2.exe  a2empty.pls > a2empty.out
+E:\GitHub\Compilers\Assignment2\Debug\Assignment2.exe  	ass2w.pls > ass2w.out
+E:\GitHub\Compilers\Assignment2\Debug\Assignment2.exe  	ass2r.pls > ass2r.out
+E:\GitHub\Compilers\Assignment2\Debug\Assignment2.exe  	a2error.pls > a2error.out
+E:\GitHub\Compilers\Assignment2\Debug\Assignment2.exe  	a2empty.pls > a2empty.out
+E:\GitHub\Compilers\Assignment2\Debug\Assignment2.exe	and.pls > and.out
+E:\GitHub\Compilers\Assignment2\Debug\Assignment2.exe	comm.pls > comm.out
+E:\GitHub\Compilers\Assignment2\Debug\Assignment2.exe	string.pls > string.out
+
