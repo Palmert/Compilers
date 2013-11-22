ebf48e9990393dd183bbd9b56ad79ea4812c068b
Thom Palmer <palm0116@algonquinlive.com>
2013-11-22 10:47:10 -0500

    Rebuilt the symbol table in master branch

--- Assignment3/Assignment3/SymbolTableTest.bat
+++
@@ -1,9 +0,0 @@
-E:\GitHub\Compilers\Assignment3\Debug\Assignment3.exe ass3m.pls > ass3m.out
-E:\GitHub\Compilers\Assignment3\Debug\Assignment3.exe ass3m.pls > ass3m.out
-E:\GitHub\Compilers\Assignment3\Debug\Assignment3.exe ass3r.pls > ass3r.out
-E:\GitHub\Compilers\Assignment3\Debug\Assignment3.exe ass3r.pls -sts:A > ass3r_stsa.out
-E:\GitHub\Compilers\Assignment3\Debug\Assignment3.exe ass3r.pls -sts:D > ass3r_stsd.out
-
-
-
-
